7c3d29abef4cde7ab5c42824af9cfc96de25acc2
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2025-09-17 22:16:10 +0530

    Create constructor.java

---
+++ constructor.java
@@ -0,0 +1,26 @@
+
+package practice;
+
+import java.util.Scanner;
+class Arman{
+	private String name;
+	
+  Arman(String name){
+	  this.name = name;
+  }
+  
+  public String getName() {
+	  return name;
+  }
+}
+
+
+public class Random{
+	public static void main(String[] args) {
+
+ Arman  abc = new Arman("Arman");
+
+	}
+
+}
+
