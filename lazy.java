5cf822a2feff7ba2292d1390fe0da020a0e293f1
Md Arman Hasan <123898576+Arman601@users.noreply.github.com>
2025-10-31 23:14:27 +0530

    Create lazy.java

---
+++ lazy.java
@@ -0,0 +1,7 @@
+class Sleepy {
+    public static void main(String[] args) {
+        System.out.println("Loading...");
+        System.out.println("Still loading...");
+        System.out.println("Okay fine, I'm too lazy to run 😴");
+    }
+}
